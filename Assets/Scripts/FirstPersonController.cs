2f19f535e16ed604a3de409b51fb61c19fe21dc8
TUFFY <emberhawk97@gmail.com>
2021-08-24 16:17:54 +0100

    Refactored and reorganised code into a more appealing and readable manner also added the audiosource helper method

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    #region Global and Instance Variables 
+
+    [Header("Game Settings")]
+    [SerializeField] [Range(8, 24)] int ammoPickupSize = 12;
+    [SerializeField] [Range(10, 100)] int healthPickupAmount = 30; 
+
+
     [Header("Player Settings")]
     public float moveSpeed = 6f;
     public float jumpHeight = 100f;
+    
 
     Rigidbody myRb;
     CapsuleCollider myCol;
@@ -65,6 +73,9 @@ public class FirstPersonController : MonoBehaviour
     int ammoDesired = 0;
     int ammoSupply = 0;
 
+    #endregion
+
+    #region Start / Initialisation
 
     // Start is called before the first frame update
     void Start()
@@ -77,13 +88,85 @@ public class FirstPersonController : MonoBehaviour
 
         currentGun.transform.SetParent(gunHandPosition);
         
-
     }
 
-    // Update is called once per frame
+    #endregion
+
+    #region Updates
+
     void Update()
     {
 
+        FootSteps();
+        ToggleHolster();
+        ReloadGun();
+        FireGun();
+        Jump();
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Look();
+        UpdateCursorLock();
+
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void Move()
+    {
+        // Movement Logic
+        delta = Time.deltaTime;
+        x = Input.GetAxis("Horizontal") * delta * moveSpeed;
+        y = Input.GetAxis("Vertical") * delta * moveSpeed;
+        
+        transform.position += cam.transform.forward * y  + cam.transform.right * x;
+    }
+
+    public void Look()
+    {
+        // Look Logic
+        float yRot = Input.GetAxis("Mouse X")  * turnSpeed;
+        float xRot = Input.GetAxis("Mouse Y")  * turnSpeed;
+
+        camRot *= Quaternion.Euler(-xRot, 0, 0);
+        playerRot *= Quaternion.Euler(0, yRot, 0);
+
+        camRot = ClampRotationAroundAxis(camRot);
+
+        transform.localRotation = playerRot;
+        cam.transform.localRotation = camRot;
+
+    }
+
+    public void Jump()
+    {
+        // Jump Logic
+        bool grounded = isGrounded();
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            myRb.AddForce(0, jumpHeight, 0);
+            PlayAudioSource(jumpSFX);
+            if(myAnim.GetBool("running"))
+            {
+                CancelInvoke(nameof(PlayFootStepAudio));
+                playingRunning = false;
+            }
+        }
+        else if(!previouslyGrounded && grounded)
+        {
+            PlayAudioSource(landSFX);
+        }
+
+        previouslyGrounded = grounded;
+    }
+
+    public void FootSteps()
+    {
         if(Mathf.Abs(x) > 0.0f || Mathf.Abs(y) > 0.0f)
         {
             if(!myAnim.GetBool("running"))
@@ -106,17 +189,23 @@ public class FirstPersonController : MonoBehaviour
             
         }
 
+    }
 
+    public void ToggleHolster()
+    {
         if(Input.GetKeyDown(KeyCode.F))
         {
             myAnim.SetBool("arm", !myAnim.GetBool("arm"));
         }
+    }
 
+    public void ReloadGun()
+    {
         if(Input.GetKeyDown(KeyCode.R) && myAnim.GetBool("arm"))
         {
             myAnim.SetTrigger("reload");
 
-            reloadSFX.Play();
+            PlayAudioSource(reloadSFX);
 
             ammoDesired = ammoMaxClip - ammoClip;
             ammoSupply = ammoDesired < ammo ? ammoDesired : ammo;
@@ -128,7 +217,10 @@ public class FirstPersonController : MonoBehaviour
             Debug.Log("Ammo in clip " + ammoClip);
             
         }
+    }
 
+    public void FireGun()
+    {
         if(Input.GetMouseButtonDown(0) && !myAnim.GetBool("fire"))
         {
             if(ammoClip > 0f ) 
@@ -139,72 +231,12 @@ public class FirstPersonController : MonoBehaviour
             }
             else if(myAnim.GetBool("arm"))
             {
-                triggerSFX.Play();
+                PlayAudioSource(triggerSFX);
             }
         }
-
-        // Jump Logic
-        bool grounded = isGrounded();
-        if(Input.GetKeyDown(KeyCode.Space) && grounded)
-        {
-            myRb.AddForce(0, jumpHeight, 0);
-            jumpSFX.Play();
-            if(myAnim.GetBool("running"))
-            {
-                CancelInvoke(nameof(PlayFootStepAudio));
-                playingRunning = false;
-            }
-        }
-        else if(!previouslyGrounded && grounded)
-        {
-            landSFX.Play();
-        }
-
-        previouslyGrounded = grounded;
-          
     }
 
-    void PlayFootStepAudio()
-    {
-        AudioSource audioSource = new AudioSource();
-
-        int n = Random.Range(1, footsteps.Length);
-
-        audioSource = footsteps[n];
-        audioSource.Play();
-        footsteps[n] = footsteps[0];
-        footsteps[0] = audioSource;
-        playingRunning = true;
-
-    }
-
-    void FixedUpdate()
-    {
-        // Movement Logic
-        delta = Time.deltaTime;
-        x = Input.GetAxis("Horizontal") * delta * moveSpeed;
-        y = Input.GetAxis("Vertical") * delta * moveSpeed;
-        
-        transform.position += cam.transform.forward * y  + cam.transform.right * x;
-        
-        // Look Logic
-        float yRot = Input.GetAxis("Mouse X")  * turnSpeed;
-        float xRot = Input.GetAxis("Mouse Y")  * turnSpeed;
-
-        camRot *= Quaternion.Euler(-xRot, 0, 0);
-        playerRot *= Quaternion.Euler(0, yRot, 0);
-
-        camRot = ClampRotationAroundAxis(camRot);
-
-        transform.localRotation = playerRot;
-        cam.transform.localRotation = camRot;
-
-
-        // Cursor Check
-        UpdateCursorLock();
-
-    }
-
+    // Normalizing a quaternion
     private Quaternion ClampRotationAroundAxis(Quaternion q)
     {
         // Normalising the incoming Quaternion
@@ -222,6 +254,7 @@ public class FirstPersonController : MonoBehaviour
 
     }
 
+    // Raycast method for ground detection
     private bool isGrounded()
     {
         RaycastHit hit;
@@ -234,7 +267,89 @@ public class FirstPersonController : MonoBehaviour
         return false;
     }
 
-    
+    #endregion
+
+    #region Events Sections
+
+    // Collisions
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(isGrounded())
+        {
+            if(myAnim.GetBool("running") && !playingRunning)
+            {
+                InvokeRepeating(nameof(PlayFootStepAudio), 0f, footStepInterval);
+            }   
+        }
+
+        if(other.gameObject.CompareTag("Ammo") && ammo < maxAmmo)
+        {
+            Destroy(other.gameObject);
+            ammo =  Mathf.Clamp(ammo + ammoPickupSize, 0, maxAmmo);
+            PlayAudioSource(ammoPickupSFX);
+            
+            Debug.Log("Got " + ammo + " ammo");
+        }
+
+        if(other.gameObject.CompareTag("MedKit") && health < maxHealth)
+        {
+            Destroy(other.gameObject);
+            health = Mathf.Clamp(health + healthPickupAmount, 0, maxHealth);
+            PlayAudioSource(healthPickupSFX);
+
+            Debug.Log("Got " + health + " hp");
+        }
+
+        if(other.gameObject.CompareTag("Lava"))
+        {
+            health -= 10;
+            if(health <= 0)
+            {
+                PlayAudioSource(deathSFX);
+                health = 0;
+            }
+            else
+            {
+                PlayAudioSource(incDamageSFX);
+            }
+
+            Debug.Log("Got " + health + " hp");   
+        }
+    }
+
+    #endregion
+ 
+    #region Audio
+
+    // Audiosource helper method
+    public void PlayAudioSource(AudioSource audioSource)
+    {
+        if(audioSource != null)
+        {
+            audioSource.Play();
+            //Debug.Log("called");
+        }
+
+    }
+
+    // Random footsep method
+    void PlayFootStepAudio()
+    {
+        AudioSource audioSource = new AudioSource();
+
+        int n = Random.Range(1, footsteps.Length);
+
+        audioSource = footsteps[n];
+        PlayAudioSource(audioSource);
+        footsteps[n] = footsteps[0];
+        footsteps[0] = audioSource;
+        playingRunning = true;
+    }
+
+
+    #endregion
+
+    #region Cursor LockState
 
     public void SetCursorLock(bool value)
     {
@@ -279,53 +394,8 @@ public class FirstPersonController : MonoBehaviour
     }
 
 
-    // Collisions
-    private void OnCollisionEnter(Collision other) 
-    {
-        if(isGrounded())
-        {
 
-            if(myAnim.GetBool("running") && !playingRunning)
-            {
-                InvokeRepeating(nameof(PlayFootStepAudio), 0f, footStepInterval);
-            }   
-        }
-
-        if(other.gameObject.CompareTag("Ammo") && ammo < maxAmmo)
-        {
-            Destroy(other.gameObject);
-            ammo =  Mathf.Clamp(ammo + 12, 0, maxAmmo);
-            ammoPickupSFX.Play();
-
-            
-            Debug.Log("Got " + ammo + " ammo");
-        }
-
-        if(other.gameObject.CompareTag("MedKit") && health < maxHealth)
-        {
-            Destroy(other.gameObject);
-            health = Mathf.Clamp(health + 50, 0, maxHealth);
-            healthPickupSFX.Play();
-
-            Debug.Log("Got " + health + " hp");
-        }
-
-        if(other.gameObject.CompareTag("Lava"))
-        {
-            health -= 10;
-            if(health <= 0)
-            {
-                deathSFX.Play();
-                health = 0;
-            }
-            else
-            {
-                incDamageSFX.Play();
-            }
-
-            Debug.Log("Got " + health + " hp");   
-        }
-    }
+    #endregion
 
 
 }
